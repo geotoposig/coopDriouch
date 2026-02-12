de1ce2097858a4b8cb30c65b8c80cb8241ed35ac
geotoposig <68599182+geotoposig@users.noreply.github.com>
2026-02-13 00:26:43 +0100

    feat: Update GeoJSON URLs
    Switched GeoJSON data sources to a new GitHub repository, updating the URLs for cooperative data and commune boundaries.

--- App.tsx
+++ App.tsx
@@ -17,8 +17,8 @@ import AIInsights from './components/AIInsights.tsx';
 
 
 // روابط البيانات الأساسية من GitHub
-const GEOJSON_URL = "https://geotoposig.com/Cooperatives_Driouch.geojson";
-const COMMUNES_BOUNDS_URL = "https://raw.githubusercontent.com/geotoposig/AIDSIG/refs/heads/main/COMMUNES_DRIOUCH.geojson";
+const GEOJSON_URL = "https://raw.githubusercontent.com/sigtopo/SIGAID/refs/heads/main/CooperativesDriouch.geojson";
+const COMMUNES_BOUNDS_URL = "https://raw.githubusercontent.com/sigtopo/SIGAID/refs/heads/main/COMMUNES_DRIOUCH.geojson";
 const PROVINCE_BOUNDS_URL = "https://raw.githubusercontent.com/geotoposig/AIDSIG/refs/heads/main/PROVINCE_DRIOUCH.geojson";
 
 const LAYERS = {
